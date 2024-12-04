1c3f76830b9fa5d97491955308bf214b34148780
rahul <tutorialcodeing@gmail.com>
2024-12-04 14:53:47 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,153 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="w-full ">
+        <div className="w-[1200px] py-[10px] mx-auto flex justify-between items-center">
+         <Image src={'https://www.swevextech.com/static/media/swevex-logo.57342302c37129db416d6a4b981ccd9b.svg'} width={200} height={120} alt=""/>
+          <ul className="flex gap-7 text-[25px]">
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="w-full mt-8">
+        <div className="home-hero-img">
+          <div className="w-[1200px] mx-auto text-[72px] font-bold ">
+            <p className="">Comprehensive </p>
+            <h3>
+              {" "}
+              <span className="text-blue-300">solution</span> For
+            </h3>
+            <h5> for your business</h5>
+            <div className="mt-[40px]">
+              <p className="text-[24px]">
+                Swevex is a ONE-STOP solution to all business, operations,{" "}
+                <br /> and execution related problems
+              </p>
+
+              <button className="p-[10px] bg-blue-300 rounded-md text-[20px]">
+                Book for demo
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-6">
+        <h1 className="text-center text-[40px] underline">Our Sevices</h1>
+      </div>
+      <div className="w-full">
+        <div className="w-[1200px] flex gap-3 flex-wrap mx-auto">
+          <div className=" borde p-[20px] ">
+            <div className="flex justify-center py-4">
+              <Image
+                src={
+                  "https://www.swevextech.com/static/media/it-infra.5b7414926b032bcaedfc2fee57a144f6.svg"
+                }
+                width={90}
+                height={90}
+                alt=""
+              />
+            </div>
+            <h1 className="text-[24px] font-bold text-center mt-[16px]">
+              IT INFRASTRUCTURE
+            </h1>
+            <p className="text-center break-words text-[19px]">
+              We enable organizations to focus on their core business operations
+              and objectives, while relying on expert support for their
+              technology needs.
+            </p>
+          </div>
+          <div className=" borde p-[20px] ">
+         <div className="flex justify-center py-4">
+         <Image src={'https://www.swevextech.com/static/media/it-infra.5b7414926b032bcaedfc2fee57a144f6.svg'} width={90} height={90} alt=""/>
+         </div>
+         <h1 className="text-[24px] font-bold text-center mt-[16px]">IT INFRASTRUCTURE</h1>
+         <p className="text-center break-words text-[19px]">We enable organizations to focus on their core business operations and objectives, while relying on expert support for their technology needs.</p>
+         </div>
+         <div className=" borde p-[20px] ">
+         <div className="flex justify-center py-4">
+         <Image src={'https://www.swevextech.com/static/media/it-infra.5b7414926b032bcaedfc2fee57a144f6.svg'} width={90} height={90} alt=""/>
+         </div>
+         <h1 className="text-[24px] font-bold text-center mt-[16px]">IT INFRASTRUCTURE</h1>
+         <p className="text-center break-words text-[19px]">We enable organizations to focus on their core business operations and objectives, while relying on expert support for their technology needs.</p>
+         </div>
+         <div className=" borde p-[20px] ">
+         <div className="flex justify-center py-4">
+         <Image src={'https://www.swevextech.com/static/media/it-infra.5b7414926b032bcaedfc2fee57a144f6.svg'} width={90} height={90} alt=""/>
+         </div>
+         <h1 className="text-[24px] font-bold text-center mt-[16px]">IT INFRASTRUCTURE</h1>
+         <p className="text-center break-words text-[19px]">We enable organizations to focus on their core business operations and objectives, while relying on expert support for their technology needs.</p>
+         </div>
+         <div className=" borde p-[20px] ">
+         <div className="flex justify-center py-4">
+         <Image src={'https://www.swevextech.com/static/media/it-infra.5b7414926b032bcaedfc2fee57a144f6.svg'} width={90} height={90} alt=""/>
+         </div>
+         <h1 className="text-[24px] font-bold text-center mt-[16px]">IT INFRASTRUCTURE</h1>
+         <p className="text-center break-words text-[19px]">We enable organizations to focus on their core business operations and objectives, while relying on expert support for their technology needs.</p>
+         </div>
+         <div className=" borde p-[20px] ">
+         <div className="flex justify-center py-4">
+         <Image src={'https://www.swevextech.com/static/media/it-infra.5b7414926b032bcaedfc2fee57a144f6.svg'} width={90} height={90} alt=""/>
+         </div>
+         <h1 className="text-[24px] font-bold text-center mt-[16px]">IT INFRASTRUCTURE</h1>
+         <p className="text-center break-words text-[19px]">We enable organizations to focus on their core business operations and objectives, while relying on expert support for their technology needs.</p>
+         </div>
+        </div>
+      </div>
+      <div className="my-6">
+        <h1 className="text-center text-[40px] underline">MILESTONES ACHIEVED</h1>
+      </div>
+      <div className="w-full">
+        <div className="w-[1200px] mx-auto flex justify-center gap-5">
+          <div>
+          <h1 className="text-[60px] font-bold">100+</h1>
+          <p className="text-[36px]">Client</p>
+          </div>
+          <div className="w-[5px] h-[100px] bg-black"></div>
+          <div>
+          <h1 className="text-[60px] font-bold">50+</h1>
+          <p className="text-[36px]">Projects</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full mt-6 bg-pink-200 py-[20px]">
+        <div className="w-[1200px]  mx-auto flex justify-between">
+            <ul className="space-y-3">
+              <h1 className="text-[20px] font-bold">Service</h1>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+             
+            </ul>
+            <ul className="space-y-3">
+            <h1 className="text-[20px] font-bold">Service</h1>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+             
+            </ul>
+            <ul className="space-y-3">
+            <h1 className="text-[20px] font-bold">Service</h1>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+             
+            </ul>
+        </div>
+      </div>
+    </>
   );
 }
